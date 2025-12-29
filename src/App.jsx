cc158eda670fb82361328a3acd8de036a301bf67
esaalhashimi <esahh222@gmail.com>
2025-12-29 15:34:52 +0300

    finish index

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import * as petService from "./services/petServic"
 
 import './App.css'
 
 function App() {
+  const [pets , setPets] = useState([])
 
+  // We only want to fetch the pets list
+  // Once when the component first mounts
+  useEffect(()=>{
+    const getAllPets = async () =>{
+      const data = await petService.index()
+      setPets(data)
+    }
+
+    getAllPets()
+  } , [])
 
   return (
     <>
